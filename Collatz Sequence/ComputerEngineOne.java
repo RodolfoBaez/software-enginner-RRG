57bb314e0e07849255899282efe5601edd606094
RodolfoBaez <47578524+RodolfoBaez@users.noreply.github.com>
2024-02-12 17:38:00 -0500

    Add files via upload

--- Collatz Sequence/ComputerEngineOne.java
+++ Collatz Sequence/ComputerEngineOne.java
@@ -1,3 +1,19 @@
-public class ComputerEngineOne {
+public class ComputerEngineOne implements CEOneInterface {
+
+    public boolean userInputRequest(String path){
+        return false;
+    }
+    public boolean csvConvertedIntoIntarr(){
+        return false;
+    }
+    public boolean intArrToDoubleArr(){
+        return false; 
+    }
+    public boolean intArrDoubleToCsv(){
+        return false; 
+    }
+    public boolean csvBackToUser(){
+        return false;
+    }
     
 }
